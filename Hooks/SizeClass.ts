d604fb38a4966b6eed6beb30b61197c2b458f045
Denis Chaschin <denis@chaschin.pro>
2023-01-25 00:36:36 +1100

    Grommet bug with responsive context initial value fixed

--- Hooks/SizeClass.ts
+++ Hooks/SizeClass.ts
@@ -1,11 +1,14 @@
-import { ResponsiveContext } from "grommet"
+import { ResponsiveContext, ThemeContext } from "grommet"
 import { useContext } from "react"
 
 const isPreRendering = () => navigator.userAgent === "ReactSnap"
 
 export const useMobile = () => {
     const size = useContext(ResponsiveContext)
-    return size === "small" || size === "xsmall" || isPreRendering()
+    const theme = useContext(ThemeContext) as { global: { breakpoints: { small: { value: number } } } }
+    const smallBreakpoint = theme.global.breakpoints.small.value ?? 768
+    const width = window.innerWidth // Grommet has a bug for the first rendering. It returns medium instead of small
+    return width <= smallBreakpoint || size === "small" || size === "xsmall" || isPreRendering()
 }
 
 export const useDesktop = () => !useMobile()
