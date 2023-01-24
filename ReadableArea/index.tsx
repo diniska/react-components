002a5dd938a50f7d19b043e37b122afa22056f5d
Denis Chaschin <denis@chaschin.pro>
2023-01-24 18:58:38 +1100

    One side safe offset support

--- ReadableArea/index.tsx
+++ ReadableArea/index.tsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
 import { Box, BoxProps, ResponsiveContext } from 'grommet'
+import { useLocale } from '../Context/LocaleContext'
+import { WritingDirection } from '../Localization'
 
 const Content = (props: BoxProps & JSX.IntrinsicElements['div']) => (
     <Box
@@ -15,7 +17,7 @@ const ReadableArea = (props: BoxProps & JSX.IntrinsicElements['div']) => (
         fill="horizontal"
         align="center"
     >
-        <Content {...props}/>
+        <Content {...props} />
     </Box>
 )
 
@@ -33,6 +35,48 @@ function styleForSize(size: string): { width: string } {
             return { width: "980px" }
     }
 
+}
+
+const ContentLeading = (props: BoxProps & JSX.IntrinsicElements['div']) => (
+    <Box
+        style={styleForSizeLeft(useContext(ResponsiveContext), useLocale().writingDirection)}
+        {...props}
+    />
+)
+
+// Displays the text paragraph that is easy to read and is offset from the leading edge
+export const ReadableAreaLeading = (props: BoxProps & JSX.IntrinsicElements['div']) => (
+    <Box
+        direction="column"
+        fill="horizontal"
+        align="start"
+    >
+        <ContentLeading {...props} />
+    </Box>
+)
+
+// A method that returns a stile of an object that has the same offset from the leading edge as styleForSize returns but doesn't have offset from the right
+function styleForSizeLeft(size: string, direction: WritingDirection): React.CSSProperties {
+    let margin: string
+
+    switch (size) {
+        case "xsmall":
+        case "small":
+            margin = "6.25%"
+            break
+        case "medium":
+            margin = "calc(50% - 346px)"
+            break
+        default:
+            margin = "calc(50% - 490px)"
+    }
+
+    switch (direction) {
+        case "rtl":
+            return { marginRight: margin }
+        case "ltr":
+            return { marginLeft: margin }
+    }
 }
 
 export default ReadableArea
