c24ac2b8a4dea5ae1bc0aa05601622e6b88bf4cc
Denis Chaschin <denis@chaschin.pro>
2023-01-29 18:53:31 +1100

    code size reduced

--- Localization/hook.ts
+++ Localization/hook.ts
@@ -22,21 +22,15 @@ export const useMultipleLocalizations = <T>(
     const locale = useLocale()
     const loader = useLocalizationsLoader()
 
-    const data = useLoadedData(useCallback(async () => {
-        if (loader === undefined) {
-            console.warn("Localizations are not available")
-            return {}
-        } else {
-            console.info("Loading localizations for", locale.code)
-            return await loader.load(locale)
-        }
-    }, [loader, locale]), `${loader?.key}_${locale.code}`)
+    const data = useLoadedData(
+        useCallback(
+            async () => loader === undefined ? {} : await loader.load(locale),
+            [loader, locale]
+        ),
+        `${loader?.key}_${locale.code}`
+    )
     
-    if (data) {
-        return keys.map(key => data[key])
-    } else {
-        return keys.map(_ => placeholder)
-    }
+    return data ? keys.map(key => data[key]) : keys.map(_ => placeholder)
 }
 
 export default useLocalized
