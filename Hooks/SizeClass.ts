6778c6550c9ccd4da8b73e6e4024b1bd4212b2a7
Denis Chaschin <denis@chaschin.pro>
2023-02-17 20:14:22 +1100

    size classes calculation improved

--- Hooks/SizeClass.ts
+++ Hooks/SizeClass.ts
@@ -1,14 +1,42 @@
 import { ResponsiveContext, ThemeContext, ThemeType } from "grommet"
 import { useContext } from "react"
 
+export type SizeClass =
+    | "xsmall"
+    | "small"
+    | "medium"
+    | "large"
+    | "xlarge"
+    | string;
+
+const defaultBreakpoints: {[key: SizeClass]: number} = {
+    xsmall: 400,
+    small: 768,
+}
+
 const isPreRendering = () => navigator.userAgent === "ReactSnap"
 
-export const useMobile = () => {
+// A safe version of useResponsiveContextSize() that works on the first rendering
+// Grommet has a bug for the first rendering. It returns medium instead of small
+export const useResponsiveContextSize = (): SizeClass => {
     const size = useContext(ResponsiveContext)
     const theme = useContext(ThemeContext) as ThemeType
-    const smallBreakpoint = theme.global?.breakpoints?.small?.value ?? 768
-    const width = window.innerWidth // Grommet has a bug for the first rendering. It returns medium instead of small
-    return width <= smallBreakpoint || size === "small" || size === "xsmall" || isPreRendering()
+
+    if (isPreRendering()) return "small"
+
+    const xsmall = theme.global?.breakpoints?.xsmall?.value ?? defaultBreakpoints.xsmall
+    const small = theme.global?.breakpoints?.small?.value ?? defaultBreakpoints.small
+    const width = window.innerWidth
+
+    return (
+        width <= xsmall ? "xsmall"
+            : width <= small ? "small"
+                : size
+    )
 }
+
+const mobileSizes: SizeClass[] = ["xsmall", "small"]
+
+export const useMobile = () => mobileSizes.includes(useResponsiveContextSize())
 
 export const useDesktop = () => !useMobile()
