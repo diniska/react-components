2fb82d6763d4b8fbfbab2127be23e009d7c08113
Denis Chaschin <denis@chaschin.pro>
2023-04-17 23:21:28 +1000

    Adding scripts instantly without page reloading

--- Analytics/index.tsx
+++ Analytics/index.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties } from "react"
+import { CSSProperties, useState } from "react"
 import CookieConsent, { getCookieConsentValue } from "react-cookie-consent"
 import GoogleAnalytics from "./GoogleAnalytics"
 import YandexMetrika, { YandexMetrikaProps } from "./YandexMetrika"
@@ -14,24 +14,34 @@ interface AnalyticsProps {
 
 /// A view that makes sure that user allowed cookies collection and only then loads analytics scripts
 /// You need to install `react-cookie-consent` package to use this component
-const Analytics = (props: AnalyticsProps) => <>{
-    isPreRendering() ||
-    <>
-        <ConsentRequest buttonStyle={{
-            padding: "8px 16px",
-            borderRadius: "8px"
-        }} />
+const Analytics = (props: AnalyticsProps) => {
+    const [consentReceived, setConsentReceived] = useState(getCookieConsentValue() !== undefined)
+
+    if (isPreRendering()) {
+        return <></>
+    }
+
+    return <>
+        <ConsentRequest
+            buttonStyle={{
+                padding: "8px 16px",
+                borderRadius: "8px"
+            }}
+            onChange={setConsentReceived}
+        />
         {
-            getCookieConsentValue() && <AnalyticsScripts {...props} />
+            consentReceived && <AnalyticsScripts {...props} />
         }
     </>
-}</>
+}
 
-const ConsentRequest = ({ buttonStyle }: { buttonStyle: CSSProperties }) =>
+const ConsentRequest = ({ buttonStyle, onChange }: { buttonStyle: CSSProperties, onChange: (consentReceived: boolean) => void }) =>
     <CookieConsent
         enableDeclineButton
         buttonStyle={buttonStyle}
         declineButtonStyle={buttonStyle}
+        onAccept={() => onChange(true)}
+        onDecline={() => onChange(false)}
     >
         This website uses cookies to analyze user experience and ads effectiveness.
         <span
