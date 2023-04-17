57dcb981d0481e84d50f821c82d9421672d71856
Denis Chaschin <denis@chaschin.pro>
2023-04-17 14:04:10 +1000

    Documentation comment improved

--- PageMeta/AlternativeUrls.tsx
+++ PageMeta/AlternativeUrls.tsx
@@ -2,14 +2,14 @@ import { Helmet } from "react-helmet"
 import { SupportedLocaleCodes } from "../Localization"
 import useWorld from "../Context/WorldContext"
 
-// Adds all localized versions of the urls to the page head
-// For example, if the page is opened at https://getchemistry.io/en/reactions/?search=H2O
-// and specified pathSuffix is `reactions/?search=H2O`
-// then the following links will be added to the head:
-// `<link rel="alternate" hrefLang="x-default" href="https://getchemistry.io/reactions/?search=H2O" />`,
-// `<link rel="alternate" hrefLang="ru" href="https://getchemistry.io/ru/reactions/?search=H2O" />`,
-// `<link rel="alternate" hrefLang="en" href="https://getchemistry.io/en/reactions/?search=H2O" />`, etc.
-// The baseUrl is taken from useWorld hook
+/// Adds all localized versions of the urls to the page head
+/// For example, if the page is opened at https://getchemistry.io/en/reactions/?search=H2O
+/// and specified pathSuffix is `reactions/?search=H2O`
+/// then the following links will be added to the head:
+/// `<link rel="alternate" hrefLang="x-default" href="https://getchemistry.io/reactions/?search=H2O" />`,
+/// `<link rel="alternate" hrefLang="ru" href="https://getchemistry.io/ru/reactions/?search=H2O" />`,
+/// `<link rel="alternate" hrefLang="en" href="https://getchemistry.io/en/reactions/?search=H2O" />`, etc.
+/// The baseUrl is taken from useWorld hook
 const AlternativeUrls = ({ pathSuffix }: { pathSuffix: string }) => {
     const baseUrl = useWorld().baseUrl
     return <Helmet>
