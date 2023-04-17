2176545d9c1d6920d317ae6b827ede01e7af369e
Denis Chaschin <denis@chaschin.pro>
2023-04-18 02:45:55 +1000

    google analytics script loading updated

--- Analytics/GoogleAnalytics.tsx
+++ Analytics/GoogleAnalytics.tsx
@@ -1,25 +1,30 @@
 import Script from "../Script"
 
-declare global {
-    interface Window {
-        dataLayer: any[]
-    }
-}
-
 interface GoogleAnalyticsProps {
     id: string
 }
 
-const GoogleAnalytics = ({ id }: GoogleAnalyticsProps) => <Script
-    async
-    keep
-    src={`https://www.googletagmanager.com/gtag/js?id=${id}`}
-    onLoad={() => {
-        window.dataLayer = window.dataLayer || []
-        const gtag = (...args: any[]) => window.dataLayer.push(args)
-        gtag("js", new Date())
-        gtag("config", id)
-    }}
-/>
+const GoogleAnalytics = ({ id }: GoogleAnalyticsProps) => {
+    const googleAnalyticsScript = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag("js", new Date());
+    gtag("config", "${id}");
+    `
+    return <>
+        <Script
+            async
+            keep
+            src={`https://www.googletagmanager.com/gtag/js?id=${id}`}
+        />
+        <Script 
+            keep
+            content={{ 
+                id: "google-analytics-setup", 
+                body: googleAnalyticsScript 
+            }}
+        />
+    </>
+}
 
 export default GoogleAnalytics
