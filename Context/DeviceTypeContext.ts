11651d4742f21d95993bc183425ee37b89efd32b
Denis Chaschin <denis@chaschin.pro>
2023-05-17 15:40:53 +1000

    Information about AppStore or Google Play presence added

--- Context/DeviceTypeContext.ts
+++ Context/DeviceTypeContext.ts
@@ -4,19 +4,21 @@ interface DeviceType {
     type: "iPhone" | "iPad" | "Android" | "unknown"
     iOS: boolean
     Android: boolean
+    // Whether AppStore or GooglePlay available on the device
+    storeAvailable: boolean
 }
 
 function currentDeviceType(): DeviceType {
     const userAgent = navigator.userAgent || navigator.vendor || (window as any).opera
 
     if (/iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
-        return { type: "iPhone", iOS: true, Android: false }
+        return { type: "iPhone", iOS: true, Android: false, storeAvailable: true }
     } else if (/iPad/.test(userAgent) && !(window as any).MSStream) { 
-        return { type: "iPad", iOS: true, Android: false }
+        return { type: "iPad", iOS: true, Android: false, storeAvailable: true }
     } else if (/android/i.test(userAgent)) {
-        return { type: "Android", iOS: false, Android: true }
+        return { type: "Android", iOS: false, Android: true, storeAvailable: true }
     } else {
-        return { type: "unknown", iOS: false, Android: false }
+        return { type: "unknown", iOS: false, Android: false, storeAvailable: false }
     }
 }
 
