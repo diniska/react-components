be02dc334e11e2cceb191899341ee3bbeaf589c2
Denis Chaschin <denis@chaschin.pro>
2024-01-21 22:48:53 +1100

    Fetch priority support added

---
+++ OptimizedImage/FetchPrioritySupport.ts
@@ -0,0 +1,10 @@
+import { HTMLAttributes } from "react"
+
+/// Adds fetchpriority attribute to img tag in React
+/// https://stackoverflow.com/a/75460919/886703
+
+declare module "react" {
+    interface ImgHTMLAttributes<T> extends HTMLAttributes<T> {
+        fetchPriority?: 'high' | 'low' | 'auto'
+    }
+}
